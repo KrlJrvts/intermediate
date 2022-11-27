6c10d9c19048415092fc4481dc01c39a08ac933b
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2022-11-27 10:46:42 +0200

    Initial release

--- 27.11/serialization.py
+++ 27.11/serialization.py
@@ -1,12 +1,18 @@
-import pickle
-
-data = {
-    "a": [1, 2.0, 3],
-    "b": ("this lesson", "is for", "Serialization"),
-    "c": [False, True, False]
-}
-
-with open("data.pickle", "wb") as f:
-    pickle.dump(data, f)
-
-print(data)
+# import pickle
+#
+# data = {
+#     "a": [1, 2.0, 3],
+#     "b": ("This lesson", "is for", "serialization"),
+#     "c": [False, True, False]
+# }
+#
+# with open("data.pickle", "wb") as f:
+#     pickle.dump(data, f)
+#
+# with open("data.pickle", "rb") as f:
+#     data = pickle.load(f)
+#
+#
+# data.close()
+#
+# print(data)
